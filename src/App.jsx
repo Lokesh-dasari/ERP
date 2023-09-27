a1b62b17b8aa4fd4e973811c7e66beae3b047c92
Lokesh-dasari <lokeshdasari25@gmail.com>
2023-09-28 02:38:24 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="bg-red-400 text-3xl font-bold underline">
       Hello world!
     </h1>
     </>
